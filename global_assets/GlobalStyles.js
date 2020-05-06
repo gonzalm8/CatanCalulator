0d2b258fc2ec8e5cb43278fc86ecd33a9256261a
gonzalm8 <gonzalm8@wwu.edu>
2020-05-05 17:54:09 -0700

    guess I have to add these files too

---
+++ global_assets/GlobalStyles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+const mainRed = "#FB3640";
+const mainBlue = "#247BA0";
+const mainWhite = "#F7F7FF";
+const mainOrange = "#F18F01";
+
+const GlobalStyles = StyleSheet.create({
+    blueCard:{
+        flex:1,
+        backgroundColor:mainBlue,
+        justifyContent:'center'
+    },
+    levelCounter:{
+        alignSelf:'center',
+        backgroundColor:mainOrange
+    },
+    buttonRight:{
+        height:40,
+        width:100,
+        backgroundColor: 'white',
+        margin:4,
+        alignItems:'flex-end'
+    }
+
+});
+
+export { GlobalStyles };
